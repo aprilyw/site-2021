c8608bd8b9dbc55d4dacbe5aff5b03624d89edcf
April <aprilyanli38@gmail.com>
2021-04-13 08:12:21 -0500

    useProgress

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,13 @@ import SEO from "../components/seo"
 import Model from "../components/grandedisco"
 import {Canvas} from "@react-three/fiber"
 import { Suspense } from 'react'
-import { OrbitControls, Html } from "@react-three/drei"
+import { OrbitControls, Html, useProgress } from "@react-three/drei"
+
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
 
 const IndexPage = () => {
   return (
@@ -11,7 +17,7 @@ const IndexPage = () => {
     <SEO title="About" />
     <Canvas style={{height: 1000,}}>
       <ambientLight intensity={1} />
-      <Suspense fallback={<Html>Loading the Grande Disco...</Html>}>
+      <Suspense fallback={<Loader />}>
         <Model />
       </Suspense>
       <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={true} autoRotate={true}/>
