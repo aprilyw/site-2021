f04f95827ae95f52793941b087b0006f9e885bbd
April <aprilyanli38@gmail.com>
2022-04-05 17:04:47 -0500

    added model back to main page for now
    quit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import SEO from "../components/seo"
+import Model from "../components/grandedisco"
+import {Canvas} from "@react-three/fiber"
+import { Suspense } from 'react'
+import { OrbitControls, Html, useProgress } from "@react-three/drei"
 
-
-
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center color="black">{progress} % loaded</Html>
+}
 
 const IndexPage = () => {
   return (
   <>
     <SEO title="Index" />
+    <div className= "">
 
+    <Canvas style={{height: 700,}}>
+      <ambientLight intensity={2} />
+      <Suspense fallback={<Loader />}>
+      <Model />
+      </Suspense>
+      <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={true} autoRotate={true}/>
+    </Canvas>
+
+
+    <p className="absolute bottom-0 right-0">
+        <strong> <i> The Grande Disco</i></strong>, 1968 <br/> 
+        Arnaldo Pomodoro <br/>
+        University of Chicago
+        
+    </p>
+    </div>
   </>
   )
 }
   
   export default IndexPage
+
+
+
+  
